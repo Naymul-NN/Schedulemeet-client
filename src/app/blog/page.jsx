ebd84db4a667540570141e8e2621625d4f5bcc5a
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-01-17 20:13:55 +0600

    second commit to merge in development branch

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -5,6 +5,9 @@ const Blog = () => {
     <div className="min-h-screen">
       This is our blog page
       <p> this p tag is added by moniruzzaman</p>
+      <p className=" bg-red-50 text-xl ">
+        this is added by moniruzzaman to pull in development branch
+      </p>
     </div>
   );
 };
