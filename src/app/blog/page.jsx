cc0ee4111c11df8bfb2947628bfcd96a1b08f6b8
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-01-17 20:08:36 +0600

    first commit by moniruzzaman

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Blog = () => {
-    return (
-        <div className='min-h-screen'>
-            This is our  blog page
-        </div>
-    );
+  return (
+    <div className="min-h-screen">
+      This is our blog page
+      <p> this p tag is added by moniruzzaman</p>
+    </div>
+  );
 };
 
 export default Blog;
