c772a3332544c7da0589bd0c71bd8bd7800ea53d
Naymul-NN <naymulislam108@gmail.com>
2024-01-17 20:10:37 +0600

    update the dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Dashboaed = () => {
     return (
         <div className='min-h-screen'>
-            this is dashboard page
+            this is our dashboard page
         </div>
     );
 };
