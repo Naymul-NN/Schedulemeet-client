1f70f48ecdebe34dbd9b1269acaf3267f3f7d451
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-01-18 21:26:01 +0600

    problem checking : checking 18-1-2023 commit  to pull

--- src/components/about/BlogValues.jsx
+++ src/components/about/BlogValues.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BlogValues = () => {
   return (
-    <div>
+    <div className=" bg-red-600 ">
       <h1>BlogValues</h1>
       <h1>BlogValues</h1>
       <h1>BlogValues</h1>
