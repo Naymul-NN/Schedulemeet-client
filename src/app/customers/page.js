9f59282315573dd45d161ba203df373689bcff2d
mdfaisalgithupe <mdfaisald0706@gmail.com>
2024-01-19 16:56:01 +0600

    update code

---
+++ src/app/customers/page.js
@@ -0,0 +1,21 @@
+
+import CustomerPages from '@/components/customers/CustomersPage';
+import React from 'react';
+
+export const metadata = {
+    title: "Customer Stories",
+    description: "Best customer service",
+  };
+
+  function CustomerPage() {
+
+  return(<>
+  
+  <CustomerPages/>
+  
+  </>)
+
+
+   }
+
+   export default CustomerPage;
