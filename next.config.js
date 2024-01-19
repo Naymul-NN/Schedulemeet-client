af208732f69c510d5cb66a9b644149d08e5ed4b0
habib33-3 <habib.swi16@gmail.com>
2024-01-19 17:17:38 +0600

    added logo in navbar

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-    images: {
-        domains: ['i.ibb.co'],
-      },
-
-}
-module.exports = nextConfig
+  images: {
+    domains: ["i.ibb.co"],
+  },
+};
+module.exports = nextConfig;
