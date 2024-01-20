2d8aa7894f853d9743a5d7f4943cba5ca52b428c
mdfaisalgithupe <mdfaisald0706@gmail.com>
2024-01-20 13:26:37 +0600

    update navabr code

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
@@ -22,7 +23,12 @@ const Navbar = () => {
                             <li><Link href="/dashboard">Dashboard</Link></li>
                         </ul>
                     </div>
-                    <Link href="/" className="btn btn-ghost text-xl">Next app</Link>
+                   <div className='flex gap-2'>
+                  
+                   <Image src="https://i.ibb.co/5LFVvdk/logo-white.webp"
+      alt="Picture of the author" width={50} height={40} />
+       <Link href="/" className="btn btn-ghost text-xl">ScheduleMeet</Link>
+                   </div>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
