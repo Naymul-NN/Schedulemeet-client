37448c91d69692b045fa2b046351e2cadcbad237
habib33-3 <habib.swi16@gmail.com>
2024-01-22 16:53:35 +0600

    fix spelling and formatting on scheduleFaster and testimonial section

--- src/components/ScheduleFaster/schedulefaster.jsx
+++ src/components/ScheduleFaster/schedulefaster.jsx
@@ -1,20 +1,29 @@
-import Title from '@/shared/Title/title';
-import React from 'react';
-import img from '../../../public/images/schedule-fast-img.webp';
-import Image from 'next/image';
+import Title from "@/shared/Title/title";
+import React from "react";
+import img from "../../../public/images/schedule-fast-img.webp";
+import Image from "next/image";
 
 const page = () => {
-    return (
-        <div className='my-12 md:my-24 flex flex-col-reverse md:flex-row gap-6 justify-center items-center'>
-                <div className='flex-1'>
-                    <Image src={img} alt='schedule fast'></Image>
-                </div>
-                <div className='flex-1 space-y-6'>
-                    <h2 className='text-2xl md:text-5xl font-bold'>Schedule Candidate Interviews, Faster</h2>
-                    <p className='text-lg font-medium text-gray-400'>Skip the back-and-forth, “just missed yous” that can delay interviews by days or even weeks. Get top talent in for interviews fast so you can hire them even faster. And when deployed with our AI Chatbot, Sense helps you prescreen AND schedule candidates automatically.</p>
-                </div>
-        </div>
-    );
+  return (
+    <div className="my-12 md:my-24 flex flex-col-reverse md:flex-row gap-6 justify-center items-center">
+      <div className="flex-1">
+        <Image
+          src={img}
+          alt="schedule fast"></Image>
+      </div>
+      <div className="flex-1 space-y-6">
+        <h2 className="text-2xl md:text-5xl font-bold">
+          Schedule Candidate Interviews, Faster
+        </h2>
+        <p className="text-lg font-medium text-gray-400">
+          Skip the back-and-forth, “just missed yous” that can delay interviews
+          by days or even weeks. Get top talent in for interviews fast so you
+          can hire them even faster. And when deployed with our AI Chatbot,
+          Sense helps you prescreen AND schedule candidates automatically.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default page;
--- src/pages/HomePage/home.jsx
+++ src/pages/HomePage/home.jsx
@@ -1,22 +1,21 @@
 import Banner from "@/components/banner/banner";
 import Faq from "@/components/faq/faq";
 import Records from "@/components/Records/records";
-import ScheduleFaster from "@/components/ScheduleFaster/schedulefaster";
+import ScheduleFaster from "@/components/ScheduleFaster/scheduleFaster";
 import Reminder from "@/components/Reminder/reminder";
 import TestimonialsSection from "@/components/HomePageSections/TestimonialsSection/TestimonialsSection";
 
-
 const Homepage = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            <Records></Records>
-            <ScheduleFaster></ScheduleFaster>
-            <Reminder></Reminder>
-            <Faq></Faq>
-            <TestimonialsSection></TestimonialsSection>
-        </div>
-    );
+  return (
+    <div>
+      <Banner></Banner>
+      <Records></Records>
+      <ScheduleFaster></ScheduleFaster>
+      <Reminder></Reminder>
+      <Faq></Faq>
+      <TestimonialsSection></TestimonialsSection>
+    </div>
+  );
 };
 
 export default Homepage;
