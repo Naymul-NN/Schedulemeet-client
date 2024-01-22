9787f3bcca1aad814d984fd7fdfa19525167ca14
Naymul-NN <naymulislam108@gmail.com>
2024-01-22 10:56:24 +0600

    dashboard layout setup

---
+++ src/app/dashboard/profile/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const profile = () => {
+    return (
+        <div>
+            this is your profile
+        </div>
+    );
+};
+
+export default profile;
---
+++ src/app/dashboard/setupinterview/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Setupinterview = () => {
+    return (
+        <div>
+            <p>set your interview</p>
+        </div>
+    );
+};
+
+export default Setupinterview;
