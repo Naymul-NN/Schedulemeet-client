4d2f3bdd0b1fe3143a266fb093522c30003ed255
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-01-23 22:38:23 +0600

    blog page : title section added , top 3 blog section added

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,13 +1,40 @@
+import TopArticleCard from "@/components/blog/TopArticleCard";
+import Title from "@/shared/Title/title";
 import React from "react";
 
 const Blog = () => {
   return (
-    <div className="min-h-screen">
-      This is our blog page
-      <p> this p tag is added by moniruzzaman</p>
-      <p className=" bg-red-50 text-xl    ">
-        this is added by moniruzzaman to pull in development branch
-      </p>
+    <div className="blogContainer bg-gray-50 text-gray-950  ">
+      <div className="blogWrapper   m-auto py-4  ">
+        {/* title section starts  */}
+
+        <Title title={"The Sense Blog"} />
+
+        <p className="   text-center font-medium text-sm sm:text-base md:text-lg pt-3 pb-5   ">
+          The latest updates, stories, ideas and guides from the Sense team.
+        </p>
+
+        {/* title section ends  */}
+
+        {/* top 3 articles section starts  */}
+        <div className="topArticles  py-9 bg-blue-100   ">
+          {/* top article title starts  */}
+
+          <div className="topArticleTitle text-xl sm:text-2xl md:text-3xl font-bold sm:font-semibold  text-center pb-8 ">
+            <h1>Explore our top articles</h1>
+          </div>
+          {/* top article title ends  */}
+
+          <div className="topArticleCardContainer w-[75%] sm:w-[90%] m-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-8  ">
+            <TopArticleCard />
+            <TopArticleCard />
+            <TopArticleCard />
+          </div>
+        </div>
+        {/* top 3 articles section ends  */}
+
+        {/*  */}
+      </div>
     </div>
   );
 };
---
+++ src/components/blog/TopArticleCard.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { FaArrowRightLong } from "react-icons/fa6";
+
+const TopArticleCard = () => {
+  return (
+    <div className="topArticleContainer   ">
+      <div className="topArticleWrapper bg-fuchsia-800 rounded-md py-4 sm:py-6 px-2 sm:px-3 group text-gray-50 ">
+        {/* title starts  */}
+
+        <h1 className="  text-lg md:text-xl font-bold py-1.5 sm:py-2 ">
+          How to send a calendar invite with Calendly
+        </h1>
+        {/* title ends  */}
+
+        {/* read time starts  */}
+        <h1 className=" py-1.5 sm:py-2 text-xs sm:text-sm md:text-base ">
+          Read time : 4 minutes{" "}
+        </h1>
+        {/* read time ends */}
+
+        {/* read more button starts  */}
+        <div className="readMore  pt-3 sm:pt-4 flex  items-center self-center gap-x-1 sm:gap-x-2    ">
+          <h1 className=" text-sm sm:text-base ">Read more </h1>
+
+          <FaArrowRightLong className=" text-base sm:text-lg group-hover:translate-x-1 transition-all duration-200  " />
+        </div>
+        {/* read more button ends  */}
+      </div>
+    </div>
+  );
+};
+
+export default TopArticleCard;
