4b1d8645a1a61092f6cb078447b88f9a9d0342fa
habib33-3 <habib.swi16@gmail.com>
2024-01-23 23:57:03 +0600

    fix folder structure and fix app breaking bug in future.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,25 @@
+import Future from "@/components/Home/Future";
+import Banner from "@/components/banner/banner";
+import Container from "@/components/container/Container";
+import Faq from "@/components/faq/faq";
+import Records from "@/components/Records/records";
+import ScheduleFaster from "@/components/ScheduleFaster/schedulefaster";
+import Reminder from "@/components/Reminder/reminder";
+import React from "react";
 
-import Future from '@/components/Home/Future';
-import Container from '@/components/container/Container';
-import Homepage from '@/pages/HomePage/home';
-import React from 'react';
-
-const page = () => {
+const Home = () => {
   return (
     <Container>
-      <div className='min-h-screen'>
-        <Homepage></Homepage>
+      <div className="min-h-screen">
+        <Banner></Banner>
+        <Records></Records>
+        <ScheduleFaster></ScheduleFaster>
+        <Reminder></Reminder>
+        <Faq></Faq>
+        <Future></Future>
       </div>
-      <Future></Future>
     </Container>
   );
 };
 
-export default page;
+export default Home;
