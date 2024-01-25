a1038014ab0f542e945fc2ec91f9e8807e66d8e5
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-01-25 23:31:53 +0600

    blog page : modified

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -38,37 +38,37 @@ const Blogs = () => {
         <div className="blogWrapper   m-auto pt-6  ">
           {/* title section starts  */}
 
-          <Title title={"The Sense Blog"} />
+          {/* <Title title={"The Sense Blog"} /> */}
 
-          <p className="   text-center font-medium text-sm sm:text-base md:text-lg pt-3 pb-5   ">
+          {/* <p className="   text-center font-medium text-sm sm:text-base md:text-lg pt-3 pb-5   ">
             The latest updates, stories, ideas and guides from the Sense team.
-          </p>
+          </p> */}
 
           {/* title section ends  */}
 
           {/* top 3 articles section starts  */}
-          <div className="topArticles  py-9 bg-blue-100   ">
-            {/* top article title starts  */}
+          {/* <div className="topArticles  py-9 bg-blue-100   "> */}
+          {/* top article title starts  */}
 
-            <div className="topArticleTitle text-xl sm:text-2xl md:text-3xl font-bold sm:font-semibold  text-center pb-8 ">
+          {/* <div className="  topArticleTitle text-xl sm:text-2xl md:text-3xl font-bold sm:font-semibold  text-center pb-8 ">
               <h1>Explore our top articles</h1>
-            </div>
-            {/* top article title ends  */}
+            </div> */}
+          {/* top article title ends  */}
 
-            <div className="topArticleCardContainer w-[75%] sm:w-[90%] m-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-8  ">
+          {/* <div className="topArticleCardContainer w-[75%] sm:w-[90%] m-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-8  ">
               <TopArticleCard />
               <TopArticleCard />
               <TopArticleCard />
-            </div>
-          </div>
+            </div> */}
+          {/* </div> */}
           {/* top 3 articles section ends  */}
 
           {/* main blog section starts  */}
 
           <div className="allBlogs   py-3 w-[90%] m-auto  ">
             {/* title starts  */}
-            <h1 className="  text-3xl text-center font-semibold py-3   ">
-              Our Latest blogs
+            <h1 className="  text-4xl text-center font-semibold py-3   ">
+              Our blogs
             </h1>
             {/* title ends  */}
 
