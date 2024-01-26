2be808584a03687d93db1abe2ab061b0e85214de
rahimatgit <rahimmonjur9@gmail.com>
2024-01-26 11:49:35 -0800

    interview and event page updated

--- src/app/dashboard/setupevents/page.jsx
+++ src/app/dashboard/setupevents/page.jsx
@@ -1,65 +1,15 @@
-import TitleDashboard from '@/shared/TitleDashboard/TitleDashboard';
+
+import SetUpEvent from '@/components/setUpEvent/setUpEvent';
 import React from 'react';
 
+
 const setupevents = () => {
+
+    
+
     return (
-        <div className='pt-20'>
-            <TitleDashboard title={"Set Your Event"}></TitleDashboard>
-            <form className="card-body">
-
-                {/* dividing the form elements */}
-                <div className='flex gap-5'>
-
-                    {/* left side elements */}
-                    <div className='flex-1'>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="email" placeholder="email" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Event Title</span>
-                            </label>
-                            <input type="event-title" placeholder="Event Title" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Host Name</span>
-                            </label>
-                            <input type="host-name" placeholder="Host Name" className="input input-bordered" required />
-                        </div>
-
-                    </div>
-
-                    {/* right side elements */}
-                    <div className='flex-1'>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Date</span>
-                            </label>
-                            <input type="date" placeholder="Date" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Time</span>
-                            </label>
-                            <input type="time" placeholder="Time" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Meeting Link</span>
-                            </label>
-                            <input type="meeting-link" placeholder="Meeting Link" className="input input-bordered" required />
-                        </div>
-
-                    </div>
-                </div>
-                <div className="form-control mt-6">
-                    <button className="btn btn-primary">Submit</button>
-                </div>
-            </form>
+        <div className='pt-10 md:pt-20 px-10 md:px-20'>
+            <SetUpEvent></SetUpEvent>
         </div>
     );
 };
---
+++ src/components/setUpEvent/SetUpEvent.jsx
@@ -0,0 +1,81 @@
+'use client'
+import React from 'react';
+import TitleDashboard from '@/shared/TitleDashboard/TitleDashboard';
+import { useForm } from "react-hook-form"
+
+const SetUpEvent = () => {
+
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm()
+     
+      const onSubmit = (data) => {
+        console.log(data);
+      } 
+
+    return (
+        <div>
+            <TitleDashboard title={"Set Your Event"}></TitleDashboard>
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+
+                {/* dividing the form elements */}
+                <div className='flex gap-5'>
+
+                    {/* left side elements */}
+                    <div className='flex-1'>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input {...register("email")} type="email" placeholder="email" className="input input-bordered text-black" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Event Title</span>
+                            </label>
+                            <input {...register("event-title")} type="text" placeholder="Event Title" className="input input-bordered text-black" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Host Name</span>
+                            </label>
+                            <input {...register("host-name")} type="text" placeholder="Host Name" className="input input-bordered text-black" required />
+                        </div>
+
+                    </div>
+
+                    {/* right side elements */}
+                    <div className='flex-1'>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Date</span>
+                            </label>
+                            <input {...register("date")} type="date" placeholder="Date" className="input input-bordered text-black" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Time</span>
+                            </label>
+                            <input {...register("time")} type="time" placeholder="Time" className="input input-bordered text-black" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Meeting Link</span>
+                            </label>
+                            <input {...register("meeting-link")} type="text" placeholder="Meeting Link" className="input input-bordered text-black" required />
+                        </div>
+
+                    </div>
+                </div>
+                <div className="form-control mt-6">
+                    <button className="btn bg-green-500 text-white">Submit</button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default SetUpEvent;
