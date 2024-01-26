a4a8a3d75ab94256690c48db826a85f4c2ab25bf
mdfaisalgithupe <mdfaisald0706@gmail.com>
2024-01-26 16:40:54 +0600

    update testimonials

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -2,53 +2,15 @@
 import Image from "next/image";
 import Marquee from "react-fast-marquee";
 import { IoMdStarOutline } from "react-icons/io";
-const Testimonials = () => {
+const Testimonials = async () => {
+
+const res = await fetch("https://schedulemeet-server-two.vercel.app//userTestimonial")
+
+const data = await res.json();
+
 
 // .....Testimonials
-const testimonialsData = [
-    {
-      username: "Naymul Islam",
-      shortDescription: "Outstanding product quality. Impressed!",
-      userImage: "https://i.ibb.co/SRpjswS/photo-1548372290-8d01b6c8e78c.jpg",
-      rating: [1, 2, 3]
-    },
-    {
-      username: "MD Ebrahim Kholilullah",
-      shortDescription: "Reliable and efficient. Would buy again!",
-      userImage: "https://i.ibb.co/cFfsRKR/photo-1560250097-0b93528c311a.jpg",
-      rating: [1, 2, 3, 4]
-    },
-    {
-        username: "হাবিবুর রহমান",
-        shortDescription: "Impressive features and user-friendly design.",
-        userImage: "https://i.ibb.co/rfBMtHV/photo-1573603867003-89f5fd7a7576.jpg",
-        rating: [1,]
-      },
-      {
-        username: "Md Moniruzzaman Billal",
-        shortDescription: "Highly recommended. Exceeded my expectations!",
-        userImage: "https://i.ibb.co/HF1rpwZ/photo-1591084728795-1149f32d9866.jpg",
-        rating: [1, 2, 3, 4]
-      },
 
-      {
-        username: "Rahim Monjur",
-        shortDescription: "Fantastic service and support!",
-        userImage: "https://i.ibb.co/XpHn4hL/photo-1617137984095-74e4e5e3613f.jpg",
-        rating: [1, 2, 3, 4]
-      },
-
-  {
-        username: "Md. Faisal",
-        shortDescription: "Great experience with the product!",
-        userImage: "https://i.ibb.co/p3xMvcz/photo-1566753323558-f4e0952af115.jpg",
-        rating: [1, 2, 3, 4]
-      },
-
-
-      
-
-  ]
 
 
 
@@ -65,7 +27,7 @@ const testimonialsData = [
 
 
 {
-    testimonialsData.map((dataAll, index) => {
+    data.result.map((dataAll, index) => {
         return(
         <div key={index} className="text-black w-[50%] mx-2 text-center bg-[#fff] shadow-lg p-10 border-[1px]">
 
