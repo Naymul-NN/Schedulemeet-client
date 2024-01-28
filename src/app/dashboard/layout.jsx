2c773767d5f3b52afbbcfb7f0a2a698692d370c1
Naymul-NN <naymulislam108@gmail.com>
2024-01-28 16:37:11 +0600

    add the dashboard icons

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,23 +1,30 @@
 import Link from "next/link";
 import "../../app/dashboard/navbar.css"
+// import { AiFillProfile } from "react-icons/ai";
+import { AiOutlineProfile } from "react-icons/ai";
+import { FiActivity } from "react-icons/fi";
+import { GrHistory, GrSchedulePlay } from "react-icons/gr";
+import { IoHomeOutline } from "react-icons/io5";
+
+
 
 const DashboardLayout = ({ children }) => {
   return (
     <div className="w-[100%] mx-auto flex text-black border-r-[1px]">
 
-      <div className="w-[20%] min-h-screen bg-[#fff] border-r-[1px] shadow-md">
+      <div className="w-[20%] min-h-screen bg-[#fff] border-r-[1px] shadow-md ">
         <header>
           <h1 className="py-3 text-center">Dashboard Header</h1>
         </header>
         <nav>
-          <ul className="navbarsStyles px-2">
-            <li><Link href="/dashboard">Activity</Link></li>
-            <li className="py-3"><Link href="/dashboard/profile">Profile</Link></li>
-            <li><Link href="/dashboard/setupinterview">Setup Interview</Link></li>
-            <li className="py-3"><Link href="/dashboard/setupevents">Setup Events</Link></li>
-            <li className="pb-3"><Link href="/dashboard/history">History</Link></li>
+          <ul className="navbarsStyles px-2 pl-4">
+            <li><Link href="/dashboard" className="hover:bg-red-500"> <FiActivity className="inline-flex mb-1 text-xl" /> Activity</Link></li>
+            <li className="py-3 "><Link href="/dashboard/profile" > <AiOutlineProfile  className="inline-flex mb-1 text-xl "/> Profile</Link></li>
+            <li><Link href="/dashboard/setupinterview"> <GrSchedulePlay className="inline-flex mb-1 text-xl" /> Setup Interview</Link></li>
+            <li className="py-3"><Link href="/dashboard/setupevents"> <GrSchedulePlay className="inline-flex mb-1 text-xl" /> Setup Events</Link></li>
+            <li className="pb-3"><Link href="/dashboard/history"> <GrHistory className="inline-flex mb-1 text-xl" /> History</Link></li>
             <hr className="mr-10"/>
-            <li className="pt-4"><Link href="/">Home</Link></li>
+            <li className="pt-4"><Link href="/"> <IoHomeOutline className="inline-flex mb-1 text-xl" />  Home</Link></li>
           </ul>
         </nav>
       </div>
