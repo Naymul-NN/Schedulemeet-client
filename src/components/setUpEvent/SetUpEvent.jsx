af0e3e896103c2de711ec1ef8bab83ce5745e582
rahimatgit <rahimmonjur9@gmail.com>
2024-02-01 19:05:23 -0800

    post operation for meeting and event create is done

--- src/components/setUpEvent/SetUpEvent.jsx
+++ src/components/setUpEvent/SetUpEvent.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import TitleDashboard from '../../shared/TitleDashboard/TitleDashboard.jsx';
 import { useForm } from "react-hook-form"
 import toast from 'react-hot-toast';
+import axios from "axios";
 
 const SetUpEvent = () => {
 
@@ -15,7 +16,18 @@ const SetUpEvent = () => {
      
       const onSubmit = (data) => {
         console.log(data);
-        toast.success('Interview scheduled successfully!');
+        axios
+      .post("http://localhost:5000/api/v1/meetings/createEvent", data)
+      .then((res) => {
+        console.log(res);
+        if (res.data.success) {
+          toast.success("Meeting added");
+        }
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+        // toast.success('Interview scheduled successfully!');
       } 
 
     return (
