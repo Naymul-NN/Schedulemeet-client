7215eb4dfdc6288aa075840a4acf183352478229
rahimatgit <rahimmonjur9@gmail.com>
2024-02-02 18:57:37 -0800

    data loading and showing on activity page done

--- src/shared/TitleDashboard/TitleDashboard.jsx
+++ src/shared/TitleDashboard/TitleDashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TitleDashboard = ({title}) => {
     return (
-        <div className='text-3xl md:text-6xl font-semibold flex justify-center text-green-500'>
+        <div className='text-3xl md:text-6xl my-8 font-semibold flex justify-center text-green-500'>
             {title}
         </div>
     );
