293074e000e0d3ca81f5bcb7e438a7c01ee283ad
habib33-3 <habib.swi16@gmail.com>
2024-02-16 13:00:00 +0600

    stripe integration

---
+++ src/app/dashboard/pay/page.jsx
@@ -0,0 +1,11 @@
+import PaymentConfig from "@/config/PaymentConfig";
+
+const PaymentPage = () => {
+  return (
+    <div>
+      <PaymentConfig />
+    </div>
+  );
+};
+
+export default PaymentPage;
---
+++ src/components/PaymentForm/PaymentForm.jsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
+
+const PaymentForm = () => {
+  const { register, handleSubmit } = useForm();
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handlePayment = async (data) => {
+    if (!stripe || !elements) return;
+
+    const card = elements.getElement(CardElement);
+
+    if (card === null) return;
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log("[error]", error);
+    } else {
+      console.log("[PaymentMethod]", paymentMethod);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(handlePayment)}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default PaymentForm;
