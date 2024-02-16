0bec7d625b75883c86c6e8c8c6a8e8e0ddbae6a8
habib33-3 <habib.swi16@gmail.com>
2024-02-16 19:25:30 +0600

    invite user

--- src/components/activity/EventDetail.jsx
+++ src/components/activity/EventDetail.jsx
@@ -5,11 +5,13 @@ import Image from "next/image";
 import toast from "react-hot-toast";
 import { useForm } from "react-hook-form";
 import useAxiosSecure from "@/components/hooks/useAxiosSecure";
+import useAuth from "@/components/hooks/useAuth";
 
 const EventDetail = ({ id }) => {
   const { event, eventsLoading } = useEvent(id);
   const axiosSecure = useAxiosSecure();
   const { register, handleSubmit } = useForm();
+  const { user } = useAuth();
 
   console.log(event);
 
@@ -36,6 +38,33 @@ const EventDetail = ({ id }) => {
     }
   };
 
+  const handleInviteUser = async (data) => {
+    const inviteInfo = {
+      invitedEmail: data.invitedEmail,
+      inviteeEmail: user?.email,
+      hostName: user?.displayName,
+      eventId: eventData._id,
+      eventTitle: eventData.eventTitle,
+      date: eventData.date,
+    };
+
+    console.log(inviteInfo);
+
+    const res = await axiosSecure.post(
+      "/api/v1/invites/inviteUser",
+      inviteInfo
+    );
+
+    if (res.status === 400) {
+      toast.error("User email doesn't exits in database");
+      return;
+    }
+
+    if (res.data.success) {
+      toast.success("Invited Successfully");
+    }
+  };
+
   return (
     <div className="pt-12 md:pt-20">
       <div className="max-w-[80%] mx-auto  card md:card-side bg-base-100 shadow-xl">
@@ -56,6 +85,13 @@ const EventDetail = ({ id }) => {
               onClick={() => document.getElementById("my_modal_4").showModal()}>
               Advertise
             </button>
+            <button
+              className="btn"
+              onClick={() => document.getElementById("my_modal_5").showModal()}>
+              Invite Users
+            </button>
+
+            {/* ad post modal */}
             <dialog
               id="my_modal_4"
               className="modal">
@@ -89,11 +125,47 @@ const EventDetail = ({ id }) => {
                       <option>5 Day</option>
                       <option>7 Day</option>
                     </select>
-
-                    <button className="btn btn-primary btn-sm my-3">
-                      Advertise
-                    </button>
                   </label>
+                  <button className="btn btn-primary btn-sm my-3 max-w-xs">
+                    Advertise
+                  </button>
+                </form>
+
+                <div className="modal-action">
+                  <form method="dialog">
+                    {/* if there is a button, it will close the modal */}
+                    <button className="btn">Close</button>
+                  </form>
+                </div>
+              </div>
+            </dialog>
+
+            {/* invite modal */}
+            <dialog
+              id="my_modal_5"
+              className="modal">
+              <div className="modal-box ">
+                <h3 className="font-bold text-lg text-black">Invite</h3>
+
+                <form
+                  className="space-y-2 card"
+                  onSubmit={handleSubmit(handleInviteUser)}>
+                  <label className="form-control w-full max-w-xs mb-1">
+                    <div className="label">
+                      <span className="label-text">Users Email</span>
+                    </div>
+                    <input
+                      type="text"
+                      placeholder="Type here"
+                      className="input input-bordered w-full max-w-xs"
+                      {...register("invitedEmail")}
+                      required
+                    />
+                  </label>
+
+                  <button className="btn btn-primary btn-sm max-w-xs my-3">
+                    Invite
+                  </button>
                 </form>
 
                 <div className="modal-action">
