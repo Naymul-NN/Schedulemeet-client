84b0d5dc469ef9251a173d70bc24937122560811
Naymul-NN <naymulislam108@gmail.com>
2024-02-18 11:24:47 +0600

    fix the single blog page

--- src/components/blog/SingleShowData.jsx
+++ src/components/blog/SingleShowData.jsx
@@ -7,7 +7,7 @@ const SingleShowData = (data) => {
 
 
 const {seeData} = data;
-const [seeDatas] = seeData    
+console.log(seeData.result)
 
     return (
         <div className="text-black bg-white min-h-screen py-10">
@@ -15,20 +15,20 @@ const [seeDatas] = seeData
   <div className="flex justify-center items-center h-full">
 <div className="w-[50%] bg-white rounded-md shadow-md p-4 my-4 border-[1px] space-y-4">
     <div className="flex items-center gap-2">
-<Image className="w-[50px]  h-[50px] rounded-full" src={seeDatas?.blogUserImage} width={100} height={100} alt="blog Image"></Image> 
+<Image className="w-[50px]  h-[50px] rounded-full" src={seeData.result?.blogUserImage} width={100} height={100} alt="blog Image"></Image> 
 <div>
-<h2>{seeDatas?.blogUserName}</h2>
-<h2 className="text-[#676767]">{seeDatas?.blogTime?.dates}-{seeDatas?.blogTime.months}-2024</h2>
+<h2>{seeData.result?.blogUserName}</h2>
+<h2 className="text-[#676767]">{seeData.result?.blogTime?.dates}-{seeData.result?.blogTime.months}-2024</h2>
 </div>
  
     </div>
 
 
 
-<h2 className="font-bold">{seeDatas?.blogTitle}</h2> 
-<Image className="block w-full h-[500px]" src={seeDatas?.blogImage} width={500} height={500} alt="blog Image"></Image> 
+<h2 className="font-bold">{seeData.result?.blogTitle}</h2> 
+<Image className="block w-full h-[500px]" src={seeData.result?.blogImage} width={500} height={500} alt="blog Image"></Image> 
 
-<p className="p-4 whitespace-pre-wrap">{seeDatas?.blogMessage}</p>
+<p className="p-4 whitespace-pre-wrap">{seeData.result?.blogMessage}</p>
 </div>
 </div>  
 
